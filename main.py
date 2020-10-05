9cc1ad57e08c5bc7b5138be73f169d62e9f66701
bochkarev <podockonnik@gmail.com>
2020-10-05 14:22:33 +0300

    Correct lengths, but graph is almost incomprehensible

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import numpy as np
 import logging
 
 def get_data(N:int=10):
+    logging.info(f"Getting top {10} contributors")
     cmd = ["./get_top.sh", str(N)]
     subprocess.Popen(cmd).wait()
+    logging.info(f"Getting all files changed by top {10} contributors")
     cmd = ["./all_files.sh", str(N)]
     subprocess.Popen(cmd).wait()
 
@@ -18,14 +20,20 @@ def parse(N:int=10):
             contributors_list.append(line.strip())
     return contributors_list
 
+def count_to_len(X:np.array, Max:int=10, Min:int=2):
+    """interpolate lineary [min, max] -> [Max, 2] (min -> Max, max -> Min)"""
+    return np.interp(X, (X.min(), X.max()), (Max, Min))
+
 def plot_graph(contributors):
-    g = Graph(f'Top {len(contributors)} contributors', filename='graph.gv', engine='neato')
-    g.attr(rankdir='LR')
+    logging.info(f"Preparing graph")
+    g = Graph(f'Top {len(contributors)} contributors', 
+              filename=f'graph_{len(contributors)}.gv', engine='neato')
 
     pairs = list(combinations(contributors, 2))
     counts = np.zeros(len(pairs), dtype=int)
     for i, (contributor1, contributor2) in enumerate(pairs):
-        cmd = ["comm", "-123", "--total", f"data/{contributor1}.txt", f"data/{contributor2}.txt"]
+        cmd = ["comm", "-123", "--total", 
+            f"data/{contributor1}.txt", f"data/{contributor2}.txt"]
         shared_changed = subprocess.run(cmd, stdout=subprocess.PIPE)
         count = int(shared_changed.stdout.decode('utf-8').split()[2])
         counts[i] = count
@@ -33,7 +41,7 @@ def plot_graph(contributors):
     top_25 = np.percentile(counts, 75)
     logging.debug(f"N contributions for top 25%: {top_25}")
 
-    lens = (np.max(counts) - counts + np.min(counts)) / np.max(counts) * 10 # some edge gets 0 length - not good
+    lens = count_to_len(counts, Max=len(contributors))
     for i, (contributor1, contributor2) in enumerate(pairs):
         if counts[i] > 0:
             if counts[i] > top_25:
@@ -45,36 +53,6 @@ def plot_graph(contributors):
 
     logging.debug(g.source)
     g.view()
-
-def plot_graph_nx(contributors):
-    G = nx.Graph()
-    labels = {}
-    for contributor in contributors:
-        G.add_node(contributor)
-        labels[contributor] = contributor
- 
-    pairs = list(combinations(contributors, 2))
-    for contributor1, contributor2 in pairs:
-        cmd = ["comm", "-123", "--total", f"data/{contributor1}.txt", f"data/{contributor2}.txt"]
-        shared_changed = subprocess.run(cmd, stdout=subprocess.PIPE)
-        count = int(shared_changed.stdout.decode('utf-8').split()[2])
-        if count > 0:
-            G.add_edge(contributor1, contributor2, weight=count)
-
-    pos = nx.circular_layout(G) # spring_layout
-    nx.draw_networkx_nodes(G, pos, node_color='red', node_size=750)
-    
-    nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
-
-    nx.draw_networkx_edges(G, pos, width=5)
-
-    edge_labels = nx.get_edge_attributes(G,'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-
-    plt.axis('off')
-    plt.title(f"Top {len(contributors)} contributors")
-    plt.savefig(f"Top_{len(contributors)}.png") 
-    plt.show() 
  
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -85,6 +63,9 @@ if __name__=='__main__':
     args = parser.parse_args()
     if args.verbose:
         logging.getLogger().setLevel(logging.DEBUG)
+    if args.number > 60:
+        logging.warning("N too large, sorry")
+        exit()
     get_data(args.number)
     contributors_list = parse(args.number)
     plot_graph(contributors_list)
